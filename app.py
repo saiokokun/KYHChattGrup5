dc96718e6062e9e1eb1a833e5d1f4802bb669a52
Zeddar <ogge1122@gmail.com>
2022-01-17 16:10:53 +0100

    Added a login page, dosen't do much right now

--- app.py
+++ app.py
@@ -15,10 +15,30 @@ def user_get():
     return render_template("user.html", name=username, friends=friends)
 
 
+@app.get("/login")
+def login_get():
+    return render_template("login.html", name=username, friends=friends)
+
+
+@app.get("/mailbox")
+def mailbox_get():
+    return render_template("user.html", name=username, friends=friends)
+
+
+@app.get("/settings")
+def settings_get():
+    return render_template("user.html", name=username, friends=friends)
+
+
 @app.post("/user")
 def user_post():
     friend = request.form["friend_name"]
     friends.append(friend)
     return redirect(url_for("user_get"))
 
-# "flask run" in the terminal
+
+@app.post("/login")
+def login_post():
+    pass
+    # tuple from the browser? or array? json?
+    # "flask run" in the terminal
